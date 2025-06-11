de7b7f896813d9a958b79094f9e4f55f07522e6c
Hashan Wijesinghe <hashan@calcey.com>
2025-06-11 13:28:42 +0530

    chore: initial commit

---
+++ src/books/books.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+
+@Controller('books')
+export class BooksController {
+  @Get()
+  getBooks() {
+    console.log('Fetching all books');
+    return [
+      { id: 1, title: '1984', author: 'George Orwell' },
+      { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+      { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    ];
+  }
+
+  @Get(':id')
+  getBookById(): string {
+    return 'Book details for the given ID';
+  }
+
+  @Post()
+  CreateBook(): string {
+    return 'Book created successfully';
+  }
+
+  @Delete(':id')
+  DeleteBook(): string {
+    return 'Book deleted successfully';
+  }
+
+  @Patch(':id')
+  UpdateBook(): string {
+    return 'Book updated successfully';
+  }
+}
